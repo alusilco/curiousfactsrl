c3526812870a8d0704da52d354ab87f429068882
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-06-07 15:42:35 +0200

    fetch/javascript

---
+++ javascipt/javaScript.js
@@ -0,0 +1,12 @@
+
+
+fetch('GET /api/v2/facts/random')
+  .then(response => response.json())
+  .then(data => {
+    // Manejar los datos obtenidos
+    console.log(data);
+  })
+  .catch(error => {
+    // Manejar cualquier error de red
+    console.error(error);
+  });
