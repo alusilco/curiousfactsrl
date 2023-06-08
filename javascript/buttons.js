dca05070994bde536991c0c9ec0f21cc0e3ba5d6
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-06-08 15:50:30 +0200

    ajuste espacios

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -4,15 +4,15 @@ function fetchData() {
   fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
     .then(response => response.json())
     .then(data => {
-      let text = data.text; // Extrae el texto del dato curioso
-      document.getElementById('data').innerText = text; // Muestra el texto en el elemento con el id "data"
-      addFavorites.push(text); // Agrega el favorito al array addFavorites
+      let text = data.text; 
+      document.getElementById('data').innerText = text; 
+      addFavorites.push(text);
     })
     .catch(error => console.log(error));
 }
 
 function saveFavorites() {
-  localStorage.setItem('addFavorites', JSON.stringify(addFavorites)); // Guarda los favoritos en localStorage
+  localStorage.setItem('addFavorites', JSON.stringify(addFavorites));
 }
 
 function showFavorites() {
