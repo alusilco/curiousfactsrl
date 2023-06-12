32c5be2af632a947cb1cac736784b6ccc94b395f
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-06-12 12:02:05 +0200

    save

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -1,30 +1,32 @@
 let addFavorites = [];
 
-    function fetchData() {
-      fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
-        .then(response => response.json())
-        .then(data => {
-          let text = data.text;
-          document.getElementById('data').innerText = text;
-          addFavorites.push(text);
-        })
-        .catch(error => console.log(error));
-    }
+function fetchData() {
+  fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
+    .then(response => response.json())
+    .then(data => {
+      let text = data.text;
+      document.getElementById('data').innerText = text;
+      if (!addFavorites.includes(text)) {
+        addFavorites.push(text);
+      }
+    })
+    .catch(error => console.log(error));
+}
 
-    function saveFavorites() {
-      localStorage.setItem('addFavorites', JSON.stringify(addFavorites));
-    }
+function saveFavorites() {
+  localStorage.setItem('addFavorites', JSON.stringify(addFavorites));
+}
 
-    
-      
-    
 // Llamar a fetchData al cargar la ventana
 window.addEventListener("load", fetchData);
 
 // Agregar evento de clic a los elementos de favoritos
 const dataElement = document.getElementById('data');
 dataElement.addEventListener('click', function() {
-const text = dataElement.innerText;
-addFavorites.push(text);
-saveFavorites(); // Guardar los favoritos después de agregar el elemento
+  const text = dataElement.innerText;
+  if (!addFavorites.includes(text)) {
+    addFavorites.push(text);
+    saveFavorites(); // Guardar los favoritos después de agregar el elemento
+  }
 });
+
